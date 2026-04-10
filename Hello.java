3c6ec4ffc2c2d79c73c03067a3db37c2799ace40
amarkarande <karandeamar45@gmail.com>
2026-04-10 19:11:32 +0530

    Thanks For Errors

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello, Tinku Sharma !!!");
+		 System.out.println("Welcome to Java Programming");
+	        System.out.println("Have a great day!");
 	}
 }
